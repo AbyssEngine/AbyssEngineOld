ded4cb62d8767fa6b91e0b64f3da9d9f4bd80fd1
Alexey Sokolov <sokolov@google.com>
2021-12-28 17:21:19 +0000

    Changes (#107)

--- apps/abyssengine/src/engine/filesystemprovider.cpp
+++ apps/abyssengine/src/engine/filesystemprovider.cpp
@@ -5,13 +5,22 @@
 
 namespace AbyssEngine {
 
+static std::string NormalizePath(const std::filesystem::path &path) {
+    // Operator / ignores the left hand path if the right hand path starts with '/', so just remove it
+    std::string p = path.string();
+    if (!p.empty() && p[0] == '/') {
+        return p.substr(1);
+    }
+    return p;
+}
+
 FileSystemProvider::FileSystemProvider(std::filesystem::path path) : _basePath(std::move(path)) {}
 
-bool FileSystemProvider::Exists(const std::filesystem::path &path) { return exists(std::filesystem::absolute(_basePath / path)); }
+bool FileSystemProvider::Exists(const std::filesystem::path &path) { return exists(std::filesystem::absolute(_basePath / NormalizePath(path))); }
 
 LibAbyss::InputStream FileSystemProvider::Load(const std::filesystem::path &path) {
     auto fb = std::make_unique<std::filebuf>();
-    fb->open(_basePath / path, std::ios_base::binary | std::ios_base::in);
+    fb->open(_basePath / NormalizePath(path), std::ios_base::binary | std::ios_base::in);
 
     return LibAbyss::InputStream(std::move(fb));
 }
